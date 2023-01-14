bd7b42643537cfb78cf7d41ddf79790116db09a1
Teammasik <ai.work.lrng@gmail.com>
2023-01-14 22:25:19 +0500

    14.01 commit

--- OpenXR/Assets/Scripts/Misc/PickupObject.cs
+++ OpenXR/Assets/Scripts/Misc/PickupObject.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PickupObject : MonoBehaviour
 {
+    [SerializeField]
+    private string objectName = "None";
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent<Player>(out var p))
         {
-            Debug.Log($"Надел {gameObject.name}");
-            //сигнал о выполнении задачи
+            Debug.Log($"Надел {objectName}!");
+            p.AddItem(objectName);
+
+
             Destroy(gameObject);
         }
     }
--- OpenXR/Assets/Scripts/Player/Player.cs
+++ OpenXR/Assets/Scripts/Player/Player.cs
@@ -1,12 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(AudioSource))]
 public class Player : MonoBehaviour
 {
-    //nothing's here
-    private void Update()
+    private List<string> Items = new List<string>();
+
+    [SerializeField]
+    private List<string> expectedItems = new List<string>();
+
+    [SerializeField]
+    private UnityEvent OnSecurityEquipped;
+
+    public bool IsReady { get; private set; }
+
+    AudioSource a_s;
+
+    private void Start()
     {
-        //Debug.Log($"{HandRayController.instance.IsLeftHolding} {HandRayController.instance.IsRightHolding}");
+        IsReady = false;
+        a_s = GetComponent<AudioSource>();
     }
+
+    [ContextMenu("Equip Everything")]
+    public void Test()
+    {
+        foreach (var item in expectedItems)
+        {
+            AddItem(item);
+        }
+    }
+
+    public void AddItem(string item)
+    {
+        Items.Add(item);
+        a_s.Play();
+
+        if (!IsReady && IsFullyEquipped())
+        {
+            IsReady = true;
+            OnSecurityEquipped?.Invoke();
+        }
+    }
+
+    private bool IsFullyEquipped()
+    {
+        foreach (var item in expectedItems)
+        {
+            if (!Items.Contains(item))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
